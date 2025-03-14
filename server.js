203649108956faf50247708258e4976429b4799f
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-03-14 16:03:38 +0100

    add routers and controllers files

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const express = require ("express");
 const app = express();
 const port = 3008
 
+// middlewere
+app.use('/api/v1/posts', routerPosts);
+
+
+//server stars listening
+app.listen(port, () =>{
+    console.log(`You're running the server on http://localhost:${port}`)
+})
