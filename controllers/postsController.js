d88b1a46040f00d73313b3b259120da0e631e430
GiuseppeFiliberto <giuseppe.filiberto.jobs@gmail.com>
2025-03-14 17:13:22 +0100

    add show route controller

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,12 +1,22 @@
 const posts = require('../data/posts')
 
+
 function index(req, res){
 
     res.json(posts)
 }
 
+function show(req, res){
 
+    const tag = req.params.tags
+
+    const singlePost = posts.find(posts => posts.tags.includes(tag))
+    console.log(singlePost);
+    
+    res.json(singlePost)
+}
 
 module.exports = {
-    index
+    index,
+    show
 }
--- routers/postsRoutes.js
+++ routers/postsRoutes.js
@@ -4,5 +4,22 @@ const postsController = require('../controllers/postsController')
 
 
 // Index route 
-
 router.get('/', postsController.index);
+
+// Show route
+router.get('/:tags', postsController.show);
+
+// Store Route
+// router.get('/', postsController.index);
+
+// Updat Route
+// router.get('/', postsController.index);
+
+// Modify Route
+// router.get('/', postsController.index);
+
+// Destroy Route
+// router.get('/', postsController.index);
+
+
+module.exports = router
